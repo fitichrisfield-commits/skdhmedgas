04f901be1cf53d80fe104f7e040fdf7f0d797003
Chrisfield Jojo Aseye Fiti <fitichrisfield@gmail.com>
2026-06-01 20:50:46 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
- 
+
 // Replace 'skdhmedgas' with your actual repo name if different
 export default defineConfig({
   plugins: [react()],
   base: "/skdhmedgas/",
 });
- 
